a05afcb90d856610fe029442b7a9fe8441bfd301
Alexandre Toledo Perez <alexandretperez@outlook.com>
2019-12-14 18:28:20 -0300

    Added new extension method XDocument.RemoveNamespaces

---
+++ Xal/Extensions/XmlExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace Xal.Extensions
+{
+    /// <summary>
+    /// Provides extension methods for XML.
+    /// </summary>
+    public static class XmlExtensions
+    {
+        /// <summary>
+        /// Removes all the namespaces from the XML elements.
+        /// </summary>
+        /// <param name="xmlDocument">The XML.</param>
+        public static void RemoveNamespaces(this XDocument xmlDocument)
+        {
+            xmlDocument.Descendants().Attributes().Where(p => p.IsNamespaceDeclaration).Remove();
+            foreach (var element in xmlDocument.Descendants())
+                element.Name = element.Name.LocalName;
+        }
+    }
+}
